d094901244cea58965e0daa57d76807146e1358e
Consep33t <agengp360@gmail.com>
2025-10-15 04:58:13 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import Navbar from './components/navBar';
+import Navbar from './components/NavBar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
